f036021b5dc4fccd34f9a2a3c740523021ad5750
Yannick Oe <yannickoetelshoven@gmail.com>
2026-07-24 19:56:41 +0200

    chore(config): drive settings from environment and wire DRF, CORS, media

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for core project.
+"""Root URL configuration for the Coderr backend project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Central routing only: the admin site plus media file serving while
+running in debug mode. Application routes live in each app's
+``api/urls`` module and are included here later.
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
